8d977ee1a357390de96f91fb1efbdba8ef90e2d5
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-16 01:01:43 +0530

    [Vidya] Add : Fibonacci Series Is Implemented

---
+++ LogicalPrograms/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+   public class FibonacciSeries
+    {
+        public void Fibonacci()
+        {
+            int num1 = 0, num2 = 1, num3, number;
+            Console.Write("Enter the number of elements: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1 + "  " + num2 + "  ");
+            for (int index = 2; index < number; index++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + "  ");
+                num1 = num2;
+                num2 = num3;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("----------Welcome To Basic Programs----------");
+            bool finish = true;
+            Console.WriteLine("\n1.Fibonacci Series\n2.End the program");
+            while (finish)
+            {
+                Console.WriteLine("\nEnter an option to execute :");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries fibo = new FibonacciSeries();
+                        fibo.Fibonacci();
+                        break;
+                    case 2:
+                        finish = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Enter a valid option !");
+                        break;
+                }
+            }
+
+        }
+    }
+}
